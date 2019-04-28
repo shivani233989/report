2bf13ef2bc99d6458e402eda8399147ffa760f80
shivani233989 <50067154+shivani233989@users.noreply.github.com>
2019-04-28 09:40:38 -0400

    commit

---
+++ instructionmemory.vhd
@@ -0,0 +1,8 @@
+
+ENTITY top_level_blk_mem_gen_0_0 IS
+  PORT (
+    clka : IN STD_LOGIC;
+    addra : IN STD_LOGIC_VECTOR(13 DOWNTO 0);
+    douta : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+  );
+END top_level_blk_mem_gen_0_0;
